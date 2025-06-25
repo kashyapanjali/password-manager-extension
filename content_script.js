83bc55e2b5c476f68961502b6153d4a36d1baf6d
kashyap_anjali <133110531+kashyapanjali@users.noreply.github.com>
2025-06-25 09:41:01 +0530

    works good

--- content_script.js
+++ content_script.js
@@ -1,19 +1,44 @@
 (function () {
-  const hostname = window.location.hostname;
-  const loginForm = document.querySelector("form");
-  const emailInput = document.querySelector("input[type='email'], input[name*='email'], input[name*='username']");
-  const passwordInput = document.querySelector("input[type='password']");
+  function autofill() {
+    const hostname = window.location.hostname;
+    const loginForm = document.querySelector("form");
+    const emailInput = document.querySelector(
+      "input[type='email'], input[name*='email'], input[name*='username'], input[name='login'], input[id='id_login']"
+    );
+    const passwordInput = document.querySelector(
+      "input[type='password'], input[name='password'], input[id='id_password']"
+    );
 
-  if (loginForm && emailInput && passwordInput) {
-    chrome.runtime.sendMessage({
-      type: "REQUEST_CREDENTIAL",
-      hostname,
-    }, function (response) {
-      if (response && response.credential) {
-        const { username, password } = response.credential;
-        emailInput.value = username;
-        passwordInput.value = password;
-      }
-    });
+    if (loginForm && (emailInput || passwordInput)) {
+      chrome.runtime.sendMessage(
+        {
+          type: "REQUEST_CREDENTIAL",
+          hostname,
+        },
+        function (response) {
+          if (response && response.credential) {
+            const { email, username, password } = response.credential;
+            if (emailInput) {
+              // Prefer email, then username
+              emailInput.value = email || username || "";
+              emailInput.dispatchEvent(new Event('input', { bubbles: true }));
+              emailInput.dispatchEvent(new Event('change', { bubbles: true }));
+            }
+            if (passwordInput) {
+              passwordInput.value = password;
+              passwordInput.dispatchEvent(new Event('input', { bubbles: true }));
+              passwordInput.dispatchEvent(new Event('change', { bubbles: true }));
+            }
+          }
+        }
+      );
+    }
   }
+
+  // Run on load
+  autofill();
+
+  // Also run if DOM changes (for dynamic forms)
+  const observer = new MutationObserver(autofill);
+  observer.observe(document.body, { childList: true, subtree: true });
 })();
